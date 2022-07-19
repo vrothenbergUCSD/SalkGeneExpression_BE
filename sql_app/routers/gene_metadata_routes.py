02b4d8f7e3a5b76e0db3439fd50aef0e4693f391
vrothenbergUCSD <vrothenberg@ucsd.edu>
2022-07-19 12:25:38 -0700

    Refactor and docstrings

---
+++ sql_app/routers/gene_metadata_routes.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.orm import Session
+
+import fetch.gene_metadata
+
+from dependencies import get_db
+
+router = APIRouter()
+
+@router.get("/gene_metadata/data")
+async def get_gene_metadata(limit: int = 100, table: str = 'mouse_trf_2018_liver_gene_metadata', db: Session = Depends(get_db)):
+    """Returns unfiltered list of gene metadata.
+
+    Args:
+        limit (int, optional): Maximum number of rows to return. Defaults to 100.
+        table (str, optional): Name of table in database. Defaults to 'mouse_trf_2018_liver_gene_metadata'.
+        db (Session, optional): Session instance of database. Defaults to Depends(get_db).
+
+    Returns:
+        list: List of gene metadata JSON row objects from database
+    """
+    return fetch.gene_metadata.get_gene_metadata(limit, table, db)
+
+@router.get("/gene_metadata/gene_name")
+async def get_gene_metadata_by_gene_name(gene_name: str, table: str = 'mouse_trf_2018_liver_gene_metadata', db: Session = Depends(get_db)):
+    """Returns filtered list of gene metadata, if gene_name in gene_names.
+
+    Args:
+        gene_name (str): List of gene names in string format e.g. Alb,Serpina3k
+        table (str, optional): Name of table in database. Defaults to 'mouse_trf_2018_liver_gene_metadata'.
+        db (Session, optional): Session instance of database. Defaults to Depends(get_db).
+
+    Returns:
+        list: List of gene metadata JSON row objects from database
+    """
+    return fetch.gene_metadata.get_gene_metadata_by_gene_name(gene_name, table, db)
+
+@router.get("/gene_metadata/chr")
+async def get_gene_metadata_by_chr(chr: str, limit: int = 100, table: str = 'mouse_trf_2018_liver_gene_metadata', db: Session = Depends(get_db)):
+    """Returns filtered list of gene metadata, if chr in chrs.
+
+    Args:
+        chr (str): List of chromosomes in string format e.g. chr7,chr8
+        limit (int, optional): Maximum number of rows to return. Defaults to 100.
+        table (str, optional): Name of table in database. Defaults to 'mouse_trf_2018_liver_gene_metadata'.
+        db (Session, optional): Session instance of database. Defaults to Depends(get_db).
+
+    Returns:
+        list: List of gene metadata JSON row objects from database
+    """
+    return fetch.gene_metadata.get_gene_metadata_by_chr(chr, limit, table, db)
